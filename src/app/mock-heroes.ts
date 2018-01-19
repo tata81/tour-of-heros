eb97ed91407d0a44071cab4e56aee9db22209d7c
tata <tatasrinath00@gmail.com>
2018-01-19 19:07:34 +0530

    Tour-of-heros first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', age: 20 },
+  { id: 12, name: 'Narco', age: 20 },
+  { id: 13, name: 'Bombasto', age: 20 },
+  { id: 14, name: 'Celeritas', age: 20 },
+  { id: 15, name: 'Magneta', age: 20 },
+  { id: 16, name: 'RubberMan', age: 20 },
+  { id: 17, name: 'Dynama', age: 20 },
+  { id: 18, name: 'Dr IQ', age: 20 },
+  { id: 19, name: 'Magma', age: 20 },
+  { id: 20, name: 'Tornado', age: 20 }
+];
